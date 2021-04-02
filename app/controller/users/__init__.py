eefc936aabada09843ed593fbf3d47c6d06455b7
Dutchie1990 <danielboone1990@gmail.com>
2021-04-02 15:10:36 +0000

    EPIC Login/Register FEAT: Register -- add register route

--- app/controller/users/__init__.py
+++ app/controller/users/__init__.py
@@ -6,3 +6,8 @@ users = Blueprint('users', __name__)
 @users.route('/login', methods=["GET", "POST"])
 def login():
     return "Here comes the login page"
+
+
+@users.route('/register', methods=["GET", "POST"])
+def register():
+    return "Here comes the register page"
